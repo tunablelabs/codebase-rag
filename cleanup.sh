53e066b07542a61c046986bde5724998db00f04e
yash1thr <yashwanth.reddy@tunablelabs.com>
2025-02-18 10:20:55 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-chmod +x cleanup.sh
+chmod +x /home/ec2-user/codebase-rag/cleanup.sh
 echo "Deleting existing files in /home/ec2-user/codebase-rag..."
 rm -rf /home/ec2-user/codebase-rag/*
 echo "Cleanup complete."
